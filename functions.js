46a4c93f63d31fb82e5d6febaf6d6363e38528be
sandilavi <sandilavihanga@gmail.com>
2023-09-06 10:33:36 +0530

    Test Contact 1 msgs display in right and 2 in left

--- functions.js
+++ functions.js
@@ -1,5 +1,5 @@
 //Function that add CCS styles for text messages
-function messageCSS(newMsgTag, newMsg) {
+function messageCSSCommon(newMsgTag, newMsg) {
     newMsgTag.appendChild(newMsg); //This will append newMsg element(mark tag) to the newMsgTag element(p tag)
     const messages = document.querySelector(".messages");
     messages.appendChild(newMsgTag);
@@ -9,9 +9,17 @@ function messageCSS(newMsgTag, newMsg) {
     newMsg.style.margin = "0px 10px 2px 10px";
     newMsg.style.padding = "8px";
     newMsg.style.maxWidth = "60%";
+    newMsg.style.whiteSpace = "pre-wrap"; //pre-wrap value allows to add multiple spaces between words
+}
+function messageCSS(newMsgTag, newMsg) {
+    messageCSSCommon(newMsgTag, newMsg);
     newMsg.style.float = "right";
     newMsg.style.clear = "right"; //Clear property makes elements line by line(As float property doesn't do it)
-    newMsg.style.whiteSpace = "pre-wrap"; //pre-wrap value allows to add multiple spaces between words
+}
+function messageCSSForLeft(newMsgTag, newMsg) {
+    messageCSSCommon(newMsgTag, newMsg);
+    newMsg.style.float = "left";
+    newMsg.style.clear = "left";
 }
 
 //Function that add the popup content on content headline
--- testContacts.js
+++ testContacts.js
@@ -1,13 +1,17 @@
-const test_contact_1_msgs = [];
-const test_contact_2_msgs = [];
+//const test_contact_1_msgs = [];
+//const test_contact_2_msgs = [];
+const test_contact_msgs   = [];
+let messageID = 0;
 
-document.querySelector("#test_contact_1").onclick = function() {
-    //Add the message content for Test Contact 1 dynamically
+function contentGenerateForTest(messageID,testContactID) {
+document.querySelector(`#test_contact_${testContactID}`).onclick = function() {
+
+    //Add the message content for Test Contacts dynamically
     const dynCon = `
         <div id="dynContent">
             <div id="headline_content" class="headline">
                 <div class="profile_pic_container ppc_content">
-                    <img src="images/num1.png" class="profile_pic">
+                    <img src="images/num${testContactID}.png" class="profile_pic">
                 </div>
                 <div class="name_container">
                     <p class="name_content"></p>
@@ -30,19 +34,26 @@ document.querySelector("#test_contact_1").onclick = function() {
     `;
     document.querySelector("#content").innerHTML = dynCon;
 
-    document.querySelector(".name_content").innerHTML = "Test Contact 1";
+    document.querySelector(".name_content").textContent = document.querySelector(`#test_contact_${testContactID} .name_contacts`).textContent;
     popupContent();
 
-    //Go through the messages in Test Contact 1
-    for (let i = 0; i < test_contact_1_msgs.length; i++) {
-        const newMsgTag = document.createElement("p");
-        const newMsg = document.createElement("mark");
+    //Go through the messages in Test Contacts
+    for (let testContactID = 1; testContactID <= 2; testContactID++) {
+        for (let i = 0; i < test_contact_msgs[testContactID-1].messages.length; i++) {
+            const newMsgTag = document.createElement("p");
+            const newMsg = document.createElement("mark");
+            newMsg.textContent = test_contact_msgs[testContactID - 1].messages[i];
 
-        newMsg.textContent = test_contact_1_msgs[i];
-        messageCSS(newMsgTag, newMsg);
+            if (testContactID == 1) {
+                messageCSS(newMsgTag, newMsg);
+            }
+            else {
+                messageCSSForLeft(newMsgTag, newMsg);
+            }
+        }
     }
     
-    //Happens after click the msg_send_btn in Test Contact 1
+    //Happens after click the msg_send_btn in Test Contacts
     document.querySelector(".msg_send_btn").onclick = sendMessage;
     document.querySelector(".enterMsg").addEventListener("keydown", function(event) {
         if (event.keyCode == 13) {
@@ -62,14 +73,33 @@ document.querySelector("#test_contact_1").onclick = function() {
 
             newMsg.textContent = enteredMsgValue;
             messageCSS(newMsgTag, newMsg);
-            test_contact_1_msgs.push(enteredMsgValue);
+
+            //test_contact_msgs[testContactID-1].msgIndex.push(messageID);
+            //test_contact_msgs[testContactID-1].contactIndex.push(testContactID);
+            test_contact_msgs[testContactID-1].messages.push(enteredMsgValue);
+            messageID++;
+
             enteredMsg.value = "";
         }
     }
 }
 
+    //Create an object to store data for Test Contacts
+    const msgLogForTestContacts = {
+        contactIndex : testContactID,
+        msgIndex : messageID,
+        messages : []
+    };
+    test_contact_msgs.push(msgLogForTestContacts);
+}
 
-document.querySelector("#test_contact_2").onclick = function() {
+//Iterate through the loop to run the contentGenerateForTest method for Test Contacts
+for (let testContactID = 1; testContactID <= 2; testContactID++) {
+    contentGenerateForTest(messageID,testContactID);
+}
+
+
+/*document.querySelector("#test_contact_2").onclick = function() {
     //Add the message content for Test Contact 2 dynamically
     const dynCon = `
         <div id="dynContent">
@@ -102,11 +132,11 @@ document.querySelector("#test_contact_2").onclick = function() {
     popupContent();
 
     //Go through the messages in Test Contact 2
-    for (let i = 0; i < test_contact_2_msgs.length; i++) {
+    for (let i = 0; i < test_contact_msgs.length; i++) {
         const newMsgTag = document.createElement("p");
         const newMsg = document.createElement("mark");
 
-        newMsg.textContent = test_contact_2_msgs[i];
+        newMsg.textContent = test_contact_msgs[i];
         messageCSS(newMsgTag, newMsg);
     }
 
@@ -130,8 +160,8 @@ document.querySelector("#test_contact_2").onclick = function() {
 
             newMsg.textContent = enteredMsgValue;
             messageCSS(newMsgTag, newMsg);
-            test_contact_2_msgs.push(enteredMsgValue);
+            test_contact_msgs.push(enteredMsgValue);
             enteredMsg.value = "";
         }
     }
-}  
+}*/  
